0c1f3ea8eacf3c60851c80914c7e679c406f7f76
Odin Hørthe Omdal <odin.omdal@gmail.com>
2017-04-16 19:13:34 +0100

    Search param removal should also work in Internet Explorer

--- assets/static/script.js
+++ assets/static/script.js
@@ -5,20 +5,31 @@ function ready() {
 }
 
 function form_feedback() {
-  var params = new URLSearchParams(location.search)
-  var success = params.get('success')
-  if (!success)
+  var p = take_param('success')
+  if (!p.value)
     return
-  var form = document.querySelector('form[data-type="'+success+'"]')
+  var form = document.querySelector('form[data-type="'+p.value+'"]')
+  if (!form)
+    return
   var msg_div = document.createElement('div')
   msg_div.textContent = form.dataset.success
   msg_div.classList = 'success hidden'
   form.appendChild(msg_div)
   requestAnimationFrame(function() { msg_div.classList.remove('hidden') })
   form.onclick = function() { msg_div.remove() }
-  params.delete('success')
-  history.replaceState(null, '', '?' + params + location.hash)
+  history.replaceState(null, '', p.search + location.hash)
 }
+
+function take_param(key) {
+  var params = new Map(location.search.slice(1).split('&')
+      .map(function(p) { return p.split(/=(.*)/) }))
+  var value = params.get(key)
+  params.delete(key)
+  var search = Array.from(params.entries()).map(
+      function(v){ return v[0]+'='+v[1] }).join('&')
+  return {search: search ? '?' + search : '', value: value}
+}
+
 
 if (['interactive', 'complete'].includes(document.readyState))
   ready()
