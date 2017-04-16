86b940335839eb80e051ec58a7d15a1fdbc3bff9
Odin Hørthe Omdal <odin.omdal@gmail.com>
2017-04-16 13:16:13 +0100

    Legg til skript

---
+++ assets/static/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function ready() {
+  form_feedback()
+}
+
+function form_feedback() {
+  var params = new URLSearchParams(location.search)
+  var success = params.get('success')
+  if (!success)
+    return
+  var form = document.querySelector('form[data-type="'+success+'"]')
+  var msg_div = document.createElement('div')
+  msg_div.textContent = form.dataset.success
+  msg_div.classList = 'success hidden'
+  form.appendChild(msg_div)
+  requestAnimationFrame(function() { msg_div.classList.remove('hidden') })
+  form.onclick = function() { msg_div.remove() }
+}
+
+console.log(document.readyState)
+if (['interactive', 'complete'].includes(document.readyState))
+  ready()
+else
+  document.addEventListener('DOMContentLoaded', ready)
