40d87ac210dd4d2b4a365cd8844925fa2f056908
Odin Hørthe Omdal <odin.omdal@gmail.com>
2017-05-13 20:56:04 +0100

    Legg til skript slik GitHub kan oppdatera sida
    Er berre webhook-delen, sj0lve skripta ligg på serveren

---
+++ assets/cgi-bin/update-staging.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import cgi
+import collections
+import os
+import subprocess
+import textwrap
+from datetime import datetime
+
+
+def _c(cmd):
+    return subprocess.check_output(cmd, shell=True, timeout=20000)
+
+def update():
+    upd = _c('sudo -Hu nmu /home/nmu/malungdom-update-from-github.sh')
+    print(textwrap.dedent('''\
+        X-Status: OK
+
+        OK
+
+        %s''') % upd.decode('utf-8'))
+
+# import cgitb; cgitb.enable()
+form = cgi.FieldStorage()
+
+meta = collections.OrderedDict(
+    ip=os.environ.get('REMOTE_ADDR'),
+    lang=os.environ.get('HTTP_ACCEPT_LANGUAGE'),
+    time=datetime.utcnow().isoformat(),
+    ua=os.environ.get('HTTP_USER_AGENT'))
+
+if os.environ['REQUEST_METHOD'] == 'POST':
+    update()
+else:
+    print('\n\nNothing')
